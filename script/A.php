6af654eaf088fc5bde407bdb32a0297d1e23a88d
bo-shev <56201331+bo-shev@users.noreply.github.com>
2021-07-18 22:25:45 +0300

    Delete script directory

--- script/A.php
+++
@@ -1,7 +0,0 @@
-<form name="auctionLot" action="B.php" method="post">
-        
-        
-        <input name="auctionend" type="datetime-local" required>
-        
-        <input type="Submit" name=confirm >
-    </form>
